e3e9da90c28ffd961a22ab8d266629f2fd232ba1
wesqu <wesquel@hotmail.com>
2022-09-26 14:34:57 -0300

    Adicionado Rota de cadastramento.

--- src/main/java/br/com/api/produtos/controllers/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controllers/ProdutoControle.java
@@ -1,7 +1,10 @@
 package br.com.api.produtos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.models.ProdutoModelo;
@@ -12,6 +15,12 @@ public class ProdutoControle {
 
     @Autowired
     private ProdutoServico ps;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrar(pm);
+    }
+
 
     @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar(){
